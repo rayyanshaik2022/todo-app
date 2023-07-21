f55e194dd41e0348bf33f91078f3cc1826b3aa8b
Rayyan Shaik <56854894+rayyanshaik2022@users.noreply.github.com>
2023-07-21 17:36:13 -0400

    Now sticky sidebar

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@ console.log("test")
 class Project {
     constructor(name) {
         this.name = name;
+        this.todos = [];
+    }
+
+    get todos() {
+        return this.todos
+    }
+
+    addTodo(todo) {
+        this.todos.push(todo);
     }
 }
+
+
+let projects = [];
+let todos = [];
